f70eb6a21b5fc976bff1b99e8f6aaff8d78231cf
Ditt Namn <DITTANV@users.noreply.github.com>
2025-10-18 23:25:18 +0200

    chore: set git identity and push

--- lib/utils/load_categories.dart
+++ lib/utils/load_categories.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
-import '../models/category.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-Future<List<Category>> loadCategoriesFromJson() async {
-  final String jsonString = await rootBundle.loadString('assets/categories.json');
-  final List<dynamic> jsonList = json.decode(jsonString);
-
-  return jsonList.map((json) => Category.fromJson(json)).toList();
+Future<List<dynamic>> loadCategories() async {
+  final jsonStr = await rootBundle.loadString('assets/data/categories.json');
+  return json.decode(jsonStr) as List<dynamic>;
 }
