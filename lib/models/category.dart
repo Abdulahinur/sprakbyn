943236f4bece41b129a90f120747e0946825bf58
Ditt Namn <DITTANV@users.noreply.github.com>
2025-10-30 20:49:42 +0100

    auto: 2025-10-30T20:49:42.7349506+01:00 on ABDULAHI

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,14 +1,16 @@
 class Category {
   final String id;
   final String title;
-  final String subtitle;
-  final String image; // asset path like assets/images/warehouse.jpg
+  final String subtitle;   // keep for future
+  final String image;
+  final String description; // NEW
 
   Category({
     required this.id,
     required this.title,
     required this.subtitle,
     required this.image,
+    required this.description,
   });
 
   factory Category.fromJson(Map<String, dynamic> json) => Category(
@@ -16,5 +18,6 @@ class Category {
         title: json['title'] as String,
         subtitle: (json['subtitle'] ?? '') as String,
         image: json['image'] as String,
+        description: (json['description'] ?? '') as String, // reads your JSON field
       );
 }
